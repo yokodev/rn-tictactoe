3e850cfba6b46aaba186a016bf09b0ae413e6315
Jesus <jcitoamaya2015@gmail.com>
2019-07-01 15:45:36 -0600

    just testing out options

--- screens/NameScreen.js
+++ screens/NameScreen.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import {View, Text } from 'react-native'
+import { useNavigation } from 'react-navigation-hooks'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { Input, Button } from 'react-native-elements'
 
-export default NameScreen =>
-  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Text>Name Screen</Text>
-  </View>
+
+export default function NameScreen(){
+  const {navigate} =useNavigation()
+
+  return (
+    <View>
+      <Text>Welcome to Gengis</Text>
+      <Input
+        placeholder="Enter the name of the first player"
+        errorStyle={{ color: 'red' }}
+        errorMessage="Need to be something"
+      />
+      <Button title="Go to TicTac" onPress={() => navigate('Tictac')} />
+      <Button title="Go to Names" onPress={() => navigate('Home')} />
+    </View>
+  )
+}
+
+
 
