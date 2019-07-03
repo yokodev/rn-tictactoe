f48850fdae0353f8f2c70ed85b32cc8e525f3484
Jesus <yokodevmaster2015@gmail.com>
2019-07-03 17:00:19 -0600

    starting fixing small issues

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,62 +1,83 @@
-import React, {useState} from "react"
-import { Input, Text, Button, Overlay } from 'react-native-elements';
-import { View, KeyboardAvoidingView, ScrollView } from 'react-native';
-import {useNavigation} from 'react-navigation-hooks'
+/*
+https://react-native-training.github.io/react-native-elements/blog/2018/12/13/react-native-web.html#0-initial-step
+https://react-native-training.github.io/react-native-elements/docs/input.html#shake
+https://facebook.github.io/react-native/docs/textinput
+https://facebook.github.io/react-native/docs/handling-text-input
+https://facebook.github.io/react-native/docs/keyboardavoidingview
+
+*/ 
+import React, { useState } from 'react'
+import { Input, Text, Button, Overlay } from 'react-native-elements'
+import { View, KeyboardAvoidingView, ScrollView } from 'react-native'
+import { useNavigation } from 'react-navigation-hooks'
+import { Formik } from 'formik'
 
 export default function GameScreen() {
-  const {navigate} =useNavigation()
-  const [visible, setVisible]= useState(true)
-  let   [players, setPlayers]= useState({x:'',o:''})
-  const [error, setError]= useState(true)
+  
+  const { navigate } = useNavigation()
+  const [visible, setVisible] = useState(true)
+  let [players, setPlayers] = useState({ x: '', o: '' })
+  const [error, setError] = useState(true)
 
-const checkInputs = ()=>{
-  if (players.x.trim().length > 0 && players.o.trim().length > 0  ){
-    setError(false) 
-    return navigate('Tictac', { players })
-  }else{
-    setError(true)
+  const checkInputs = () => {
+    if (players.x.trim().length > 0 && players.o.trim().length > 0) {
+      setError(false)
+      return navigate('Tictac', { players })
+    } else {
+      setError(true)
+    }
   }
-}
+
   return (
-    <ScrollView>
-      <View style={{ flex: 1 }}>
-        <Overlay isVisible={visible} height="auto" >
-          <View>
-            <Text style={{ fontWeight: 'bold' }}>Welcome to Gengis....</Text>
-            <Text>In the following inputs type in the name of the players.</Text>
-            <Text>One for X and one for 0 </Text>
-            <Text>Good Luck!!! </Text>
-            <Button title='Got it!' containerStyle={{ padding: 10 }} onPress={() => setVisible(false)} />
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled >
+      <ScrollView>
+        <View style={{ flex: 1 }}>
+          <Overlay isVisible={visible} height="auto">
+            <View>
+              <Text style={{ fontWeight: 'bold' }}>Welcome to Gengis....</Text>
+              <Text>
+                In the following inputs type in the name of the players.
+              </Text>
+              <Text>One for X and one for 0 </Text>
+              <Text>Good Luck!!! </Text>
+              <Button
+                title="Got it!"
+                containerStyle={{ padding: 10 }}
+                onPress={() => setVisible(false)}
+              />
+            </View>
+          </Overlay>
+          <View style={{ alignItems: 'center', padding: 10 }}>
+            <Text h2>Welcome to Gengis</Text>
           </View>
-        </Overlay>
-        <View style={{ alignItems: 'center', padding: 10 }}>
-          <Text h2 >Welcome to Gengis</Text>
+          <Formik>
+            <View style={{ padding: 40 }}>
+              <Input leftIcon={{ name: 'alpha-x-circle', type: 'material-community' }}
+                autoFocus={true}
+                placeholder="First player"
+                errorStyle={{ color: 'red' }}
+                errorMessage={"Input can't be empty!" }
+                shake={true}
+                name="x"
+                onChangeText={text => setPlayers({ ...players, x: text })}
+                value={players.x}
+              />
+              <Input leftIcon={{ name: 'numeric-0-circle', type: 'material-community' }}
+                placeholder="Second player"
+                errorStyle={{ color: 'red' }}
+                errorMessage={"Input can't be empty!"}
+                shake={true}
+                name="o"
+                onChangeText={text => setPlayers({ ...players, o: text })}
+                value={players.o}
+              />
+              <Button title="Submit" onPress={handleSubmit} />
+              {/* <Button title="Submit" onPress={() => checkInputs()} /> */}
+              {/* <Button title="Submit" onPress={()=>alert(players.x,players.o)} /> */}
+            </View>
+          </Formik>
         </View>
-        <View style={{ padding: 40 }}>
-          <Input
-            leftIcon={{ name: 'alpha-x-circle', type: 'material-community' }}
-            placeholder='First player'
-            errorStyle={{ color: 'red' }}
-            errorMessage={error ? "Input can't be empty!":null}
-            shake={error  ? true : false}
-            name='x'
-            onChangeText={(text) => setPlayers({ ...players, x: text })}
-            value={players.x}
-          />
-          <Input
-            leftIcon={{ name: 'numeric-0-circle', type: 'material-community' }}
-            placeholder='Second player'
-            errorStyle={{ color: 'red' }}
-            errorMessage={error ? "Input can't be empty!":null}
-            shake={error ? true : false}
-            name='o'
-            onChangeText={(text) => setPlayers({ ...players, o: text })}
-            value={players.o}
-          />
-          <Button title="Submit" onPress={() =>checkInputs()} />
-          {/* <Button title="Submit" onPress={()=>alert(players.x,players.o)} /> */}
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
